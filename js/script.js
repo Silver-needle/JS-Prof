55bc01937903c685378eebfda3ec71e97d0f1093
Саморай Маргарита <samorai_m_qschool@qsoft.ru>
2023-12-09 15:07:29 +0300

    0.2

--- js/script.js
+++ js/script.js
@@ -3,42 +3,70 @@
 const goods = [
    {
       id: 1,
-      title: 'Personal computer v 4', 
+      title: 'Personal computer v 4',
       price: 1600,
       description: 'Some quick example text to build on the card title and make up the bulk of the card is content.',
       img: "./css/img/2.jpg",
    },
    {
       id: 2,
-      title: 'Personal computer v 3', 
+      title: 'Personal computer v 3',
       price: 800,
       description: 'Some quick example text to build on the card title and make up the bulk of the card is content.',
       img: "./css/img/3.jpg",
    },
    {
       id: 3,
-      title: 'Macintosh 128k', 
+      title: 'Macintosh 128k',
       price: 1400,
       description: 'Some quick example text to build on the card title and make up the bulk of the card is content.',
       img: "./css/img/4.jpg",
    },
+   {
+      id: 4,
+      title: 'Incognito PC',
+      price: 400,
+      description: 'Some quick example text to build on the card title and make up the bulk of the card is content.',
+   }
 ];
 
-const renderGoodsItem = ({ title = '', price = 0, description = '', img = "./css/img/5.jpg" }) =>
-`<div class="card" style="width: 18rem;">
-         <img src="${img}" class="card-img-top" style="padding-top: 20px;">
-         <div class="card-body">
-            <h5 class="card-title">${title}</h5>
-            <p class="card-text">${description}</p>
-            <h3 class="product-price">${price} $</h3>
-            <a href="#" class="btn btn-primary">Add to Cart</a>
-         </div>
-      </div>`
-;
+class GoodsItem {
+   constructor({ title = '', id = 0, price = 0, description = '', img = "./css/img/5.jpg" }) {
+      this.id = id;
+      this.title = title;
+      this.price = price;
+      this.description = description;
+      this.img = img;
+   }
+   render() {
+      return `
+      <div class="card" style="width: 18rem;">
+      <img src="${this.img}" class="card-img-top" style="padding-top: 20px;">
+      <div class="card-body">
+         <h5 class="card-title">${this.title}</h5>
+         <p class="card-text">${this.description}</p>
+         <h3 class="product-price">${this.price} $</h3>
+         <a href="#" class="btn btn-primary">Add to Cart</a>
+      </div>
+   </div>
+   `
+   }
+}
+class GoodsList {
+   items = [];
+   fetchGoods() {
+      this.items = goods;
+   }
+   render() {
+      const goods = this.items.map(item => {
+         const goodItem = new GoodsItem(item);
+         return goodItem.render()
+      }).join('');
 
-const renderGoodsList = (list = []) => {
-   let goodsList = list.map(item => renderGoodsItem(item));
-   document.querySelector('.card-container').innerHTML = goodsList.join('');
+      document.querySelector('.card-container').innerHTML = goods;
+   }
 }
 
-renderGoodsList(goods); 
+const goodsList = new GoodsList();
+goodsList.fetchGoods();
+goodsList.render();
