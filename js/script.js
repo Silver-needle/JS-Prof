5d96d2639c76b79cc960be8dfe0a7d66de6a2c8d
Margarita Samoray <msamoray161@gmail.com>
2024-01-14 00:51:04 +0300

    0.1

--- js/script.js
+++ js/script.js
@@ -31,68 +31,129 @@ const goods = [
 ];
 */
 const BASE_URL = 'http://localhost:8000/';
-const GET_GOODS_ITEMS = `${BASE_URL}goods`;
-// const GET_BASKET_GOODS_ITEMS = `${BASE_URL}getBasket.json`
+const GET_GOODS_ITEMS = `${BASE_URL}/goods.json`;
+const GET_BASKET_GOODS_ITEMS = `${BASE_URL}/basket-goods.json`
 
-function service(url) {
-   return fetch(url)
+function service(url, method = "GET", body) {
+   return fetch(url, {
+      headers: Object.assign({}, body ? {
+         'Content-Type': 'application/json; charset=utf-8'
+      } : {}),
+      method,
+      body: JSON.stringify(body)
+   })
       .then((res) => res.json())
 }
 function init() {
-   const CustomButton = Vue.component('custom-button', {
-     template: `
-       <button class="search-button" type="button" v-on:click="$emit('click')">
-          <slot></slot>
-       </button>
-     `
+   Vue.component('search-component', {
+      model: {
+         prop: 'value',
+         event: 'input'
+      },
+      props: {
+         value: String,
+      },
+      template: `
+         <input type="text" class="goods-search" : value="value" @input = "$emit('input', $event.terget.value)" />
+   `
    })
-const basketGoods = Vue.component('basket-goods', {
-   data() {
-     return {
-        basketGoodsItems: []
-     }
-   },
+
+   const CustomButton = Vue.component('custom-button', {
+      template: `
+         <button class="search-button" type="button" v-on:click="$emit('click')">
+            <slot></slot>
+         </button>
+      `
+   })
+   const basketGoods = Vue.component('basket-goods', {
+      data() {
+         return {
+            basketGoodsItems: []
+         }
+      },
 
       template: `
-        <div class="fixed-area">
-           <div class="basket-card">
-              <div class="basket-card__header">
-                 <h1 class="basket-card__header__title">basket card</h1>
-                 <div class="basket-card__header__delete-icon"
-                    @click="$emit('close')"
-                 ></div>
-              </div>
-              <div class="basket-card__content">
-                 content
-              </div>
-           </div>
-        </div>
-        `,
-        mounted() {
-        }
-    });
+         <div class="fixed-area">
+            <div class="basket-card">
+               <div class="basket-card__header">
+                  <h1 class="basket-card__header__title">basket card</h1>
+                  <div class="basket-card__header__delete-icon"
+                     @click="$emit('close')"
+                  ></div>
+               </div>
+               <div class="basket-card__content">
+                  <basket-goods-item v-for="item in basketGoodsItems" :item="item" @delete-"deleteBasketGood" @add="addGood"></basket-goods-item>
+               </div>
+            </div>
+         </div>
+         `,
+      mounted() {
+         service(GET_BASKET_GOODS).then((data) => {
+            this.basketGoodsItems = data
+         })
+      },
+      methods: {
+         deleteBasketGood(id) {
+            service(GET_BASKET_GOODS, "DELETE", {
+               id
+            }).then((data) => {
+               this.basketGoodsItems = data
+            })
+         },
+         addGood() {
+            service(GET_BASKET_GOODS, 'PUT', {
+               id
+            }).then((data) => {
+               this.basketGoodsItems = data
+            })
+         }
+      }
+   })
 
 
-   const goodsItem =Vue.component('card', {
+   Vue.component('basket-goods-item', {
       props: [
          'item'
       ],
       template: `
-      <div class="card" style="width: 18rem;">
-        <img src="/css/img/5.jpg" class="card-img-top" style="padding-top: 20px;"><br>
-        <div class="card-body">
-           <h5 class="card-title"> {{ item.product_name }}</h5>
-           <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card is content.</p>
-           <h3 class="product-price">{{ item.price }} $</h3>
-           <a href="#" class="btn btn-primary">Add to Cart</a>
-        </div>
-        </div>
-    `
+      <div class="card basket-card__content___item" style="width: 18rem;">
+         <img src="/css/img/5.jpg" class="card-img-top" style="padding-top: 20px;"><br>
+         <div class="card-body">
+            <h5 class="card-title"> {{ item?.data?.product_name }}</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card is content.</p>
+            <div>count: {{item?.count}}</div>
+            <h3 class="product-price">{{ item?.price }} $</h3>
+            <div>total: {{item?.total}}</div>
+            <custom-button class="btn btn-primary" @click="$emit('add', item.data.id)">Add to Cart</custom-button>
+            <custom-button class="btn btn-primary" @click="$emit('delete', item.data.id)">Delete</custom-button>
+         </div>
+      `
    });
+
+   const goodsItem = Vue.component('goods-item', {
+      props: [
+         'item'
+      ],
+      template: `
+   <div class="goods-item">
+   <h3>{{item.product_name}}</h3>
+   <p>{{item.price}}</p>
+   <custom-button @click="addGood">Add</custom-button>
+   </div>
+   `,
+      methods: {
+         addGood() {
+            service(GET_BASKET_GOODS, 'PUT', {
+               id: this.id
+            })
+         }
+      }
+   })
+
    Vue.component('search', {
       template: `
       <input type="text" class="goods-search" style="margin-right: 10px; border-radius: 6px;" v-model="$root.search" />
-    `
+      `
    });
    const app = new Vue({
       el: '#root',
@@ -102,7 +163,7 @@ const basketGoods = Vue.component('basket-goods', {
          search: '',
          isVisibleCart: false
       },
-      
+
       methods: {
          fetchGoods() {
             setTimeout(() => {
@@ -134,31 +195,6 @@ const basketGoods = Vue.component('basket-goods', {
    })
 }
 window.onload = init
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 /*
 function service(url, callback) {
